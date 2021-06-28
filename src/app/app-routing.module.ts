e488b4c3aee19e5560cf0049f24bd65ba213bd2a
Yehuda Buzagllo <yehudab@codevalue.com>
2021-06-28 15:12:40 +0300

    added components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './auth/auth-guard.service';
+import { ChatroomContainerComponent } from './components/chatroom-container/chatroom-container.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'chat',
+    component: ChatroomContainerComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: 'chat/:roomname',
+    component: ChatroomContainerComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
